be4c4180717429fdd4bca3c58ae05c50d9e7bcda
fatameri@hotmail.com <amir4767>
2018-03-17 00:58:23 -0400

    first step

--- SelectionSort/src/SelectionSort.java
+++ SelectionSort/src/SelectionSort.java
@@ -4,6 +4,12 @@ public class SelectionSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] mySort= {1,4,7,3,5};
+		while( mySort.length !=0)
+		{
+			int min=mySort[0];
+			//for (int i=i+1 ;i<=)
+		}
 	}
 
 }
