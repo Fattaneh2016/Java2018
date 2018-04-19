999334bf2813d9e05e49866e5a2caf58db74d6c4
Fattaneh <fatameri@hotmail.com>
2018-04-19 16:19:23 -0400

    Add files via upload
    interface 1

---
+++ TriangleClient.java
@@ -0,0 +1,17 @@
+
+public class TriangleClient {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		EquyilateralTriangle t=new EquyilateralTriangle(4, 4, 7);
+				
+		t.printShape();
+		t.move(8, 5);
+		t.printShape();
+		
+	}
+
+}
