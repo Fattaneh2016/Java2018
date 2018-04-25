bcca9c7d13e35c753819286ada786d692d49633b
Fattaneh <fatameri@hotmail.com>
2018-04-24 20:19:04 -0400

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -1,35 +1,21 @@
+
 public class Rectangle {	
-	private int width;
-	private int height;
-	private int x;
-	private int y;
+	int width;
+	int Height;
+	int X;
+	int Y;
 	
-	public void setfields(int newX,int newY,int newWidth,int newHeight){
-		x=newX;
-		y=newY;
-		width=newWidth;
-		height=newHeight;
-	}	
-	public Rectangle(int initialX, int initialY, int initialWidth, int initialHeight){		
-		x=initialX;
-		y=initialY;
-		width=initialWidth;
-		height=initialHeight;		
-	}	
-	public boolean contains(int x,int y){		
-		if( this.x <=x && this.y<= y && (this.x + this.width)>= x && (this.y + this.height)>= y ){
-			return true;
-		}else {
-			return false;
-		}		
+	public void setfields(int newx,int newy,int newwidth,int newheight){
+
+		X=newx;
+		Y=newy;
+		width=newwidth;
+		Height=newheight;
 	}
-	//returns a string such as {(5,12), 4x8}
-	public String toString(){
-		return "{( " + x + "," + y + ")" + "," + width + "x" + height + "}";
-	}
+	
 	public int getHeight(){
 
-		return height;
+		return Height;
 	}
 	public int getWidth(){
 
@@ -37,10 +23,15 @@ public class Rectangle {
 	}
 	public int getX(){
 
-		return x;
+		return X;
 	}
 	public int getY(){
 
-		return y;
-	}	
+		return Y;
+	}
+	public int getArea(){
+
+		return width*Height;
+	}
+
 }
