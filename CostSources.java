295e00f84f512d87583ad0f6c65aee8e0999026f
Fattaneh <fatameri@hotmail.com>
2018-04-26 21:56:32 -0400

    Add files via upload

---
+++ CostSources.java
@@ -0,0 +1,20 @@
+
+
+public class CostSources {
+
+	private double amount;
+
+	public CostSources(double initAmount) {	
+
+		this.amount=initAmount;
+	}	
+
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+}
+
+
